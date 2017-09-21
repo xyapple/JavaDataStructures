1f393d69ab9dd3bdcd70358ca6e7dac584b7ae39
xyapple <ydeascentis@gmail.com>
2017-09-21 18:48:11 -0400

    Create a Reverse Linked List

---
+++ src/Recursion/LinkedListReverser.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LinkedListReverser {
+    /**
+     * Reverses a linked list
+     * @param head the linked list to reverse
+     * @return head of the reverse linked list
+     */
+
+
+    Node reverseLinkedList(Node head){
+
+        //list is empty 0
+        if(head == null || head.getNext() == null){
+            return head;
+        }
+       Node newHead = reverseLinkedList(head.getNext());
+
+        //list only has 1 element
+       head.getNext().setNext(head);
+       head.setNext(null);
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        //create a linked list
+        LinkedListCreator creator = new LinkedListCreator();
+        //reverse a linked list
+        LinkedListReverser reverser= new LinkedListReverser();
+
+        //create a empty list
+        Node.printLinkedList(reverser.reverseLinkedList(creator.createLinkedList((new ArrayList<>()))));
+        //create a list has 1 element
+        Node.printLinkedList(reverser.reverseLinkedList(creator.createLinkedList(Arrays.asList(1))));
+        //create a list has a few elements
+        Node.printLinkedList(reverser.reverseLinkedList(creator.createLinkedList(Arrays.asList(1,2,3,4,5))));
+
+
+
+    }
+}
