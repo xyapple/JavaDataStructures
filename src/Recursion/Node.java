4e66d1f7140558e2567b0d50ecb193a0446f38d7
xyapple <ydeascentis@gmail.com>
2017-09-21 17:55:18 -0400

    Create a new java class Node

---
+++ src/Recursion/Node.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class Node {
+    //
+    private final int value;
+    //
+    private Node next;
+
+    //construtor
+    public Node(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+    public Node getNext(){
+        return next;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+
+}
