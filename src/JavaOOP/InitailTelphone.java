5a960aebdd3f2ddfa508098082e2a97279497807
xyapple <ydeascentis@gmail.com>
2017-09-24 10:29:47 -0400

    Learn about Java OOP basced

---
+++ src/JavaOOP/InitailTelphone.java
@@ -0,0 +1,7 @@
+package JavaOOP;
+
+public class InitailTelphone {
+    public static void main(String[] args) {
+        Telphone phone = new Telphone();
+    }
+}
---
+++ src/JavaOOP/Telphone.java
@@ -0,0 +1,7 @@
+package JavaOOP;
+
+public class Telphone {
+    public Telphone(){
+        System.out.println("Print what Telphone Class has");
+    }
+}
