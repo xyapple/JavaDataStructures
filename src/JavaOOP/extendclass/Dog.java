49b526248229656bafd2640fc43466e188377c51
xyapple <ydeascentis@gmail.com>
2017-09-25 08:25:13 -0400

    Rewrite the Animal class, public method in the Dog class

--- src/JavaOOP/extendclass/Dog.java
+++ src/JavaOOP/extendclass/Dog.java
@@ -1,5 +1,9 @@
 package JavaOOP.extendclass;
 //this class is extended Class of Animal
 public class Dog extends Animal {
+    //rewrite the Animal class method
+    public void hasLegs(){
+        System.out.println("name: "+this.name+" age: "+this.age+" Dog has legs.");
+    }
 
 }
