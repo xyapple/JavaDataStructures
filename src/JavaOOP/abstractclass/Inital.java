73aabbf18add188138a79f38fa24d144ddfeb66b
xyapple <ydeascentis@gmail.com>
2017-09-25 11:22:00 -0400

    Understand the Interface and abstract classes

--- src/JavaOOP/abstractclass/Inital.java
+++ src/JavaOOP/abstractclass/Inital.java
@@ -2,6 +2,7 @@ package JavaOOP.abstractclass;
 
 public class Inital {
     public static void main(String[] args) {
+        System.out.println("============== Abstract Class =======");
         Telphone cell = new cellphone();
         cell.call();
         cell.message();
@@ -9,6 +10,20 @@ public class Inital {
         Telphone smart = new smartPhone();
         smart.call();
         smart.message();
+        System.out.println("============== Interface Part 1=========");
+        IPlayGame playgame1 = new smartPhone();
+        playgame1.playGame();
+        IPlayGame playGame2 = new Psp();
+        playGame2.playGame();
+        System.out.println("============== Interface Part 2 =========");
+        IPlayGame playGame3 = new IPlayGame() {
+            @Override
+            public void playGame() {
+                System.out.println("Use the Interface anonymous method");
+            }
+        };
+        playGame3.playGame();
+        
     }
     
 }
---
+++ src/JavaOOP/abstractclass/Psp.java
@@ -0,0 +1,7 @@
+package JavaOOP.abstractclass;
+
+public class Psp implements IPlayGame{
+    public void playGame(){
+        System.out.println("PSP also can play video game. ");
+    }
+}
