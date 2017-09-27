6cd4a4504ac3ebbb9fff97a17ffcd0a9f4b40311
xyapple <ydeascentis@gmail.com>
2017-09-27 11:29:11 -0400

    Overwrite the equals() in the Course Class then, the SelectCourse.java had been changed from 'false' to 'true'.

--- Map_Demo/src/collection/Course.java
+++ Map_Demo/src/collection/Course.java
@@ -17,4 +17,28 @@ public class Course {
     public Course(){
     
     }
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){return false;}
+        if(!(obj instanceof Course)){
+            return false;
+
+        }
+        Course course = (Course)obj;
+        if(this.name == null){
+           if(course.name == null){
+               return true;
+           } else {
+               if(this.name.equals(course.name)){
+                   return true;
+               } else {
+                   return false;
+               }
+           }
+        }
+        return true;
+    
+    }
 }
