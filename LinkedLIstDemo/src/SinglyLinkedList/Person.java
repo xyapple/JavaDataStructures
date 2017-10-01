ecf9e530cbed8c4f84c245a37b6025b6021db2a8
xyapple <ydeascentis@gmail.com>
2017-10-01 14:53:07 -0400

    Create the Person Class which is the father of class

---
+++ LinkedLIstDemo/src/SinglyLinkedList/Person.java
@@ -0,0 +1,61 @@
+package SinglyLinkedList;
+
+class Person {
+    //Instance variables
+    private String name;
+    private String email;
+    private int age;
+    
+    //constructor
+    public Person(String name, String email, int age){
+        this.name = name;
+        this.email = email;
+        assert age > 0;
+        this.age = age;
+    }
+    
+    //methods
+    public String getName(){
+        return name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int setAge(int age) {
+        
+        if(age < 0){
+            System.out.println("Sorry, person's age cannot be negative. Person record will not change.");
+            return 0;
+            
+        } else if(age >= 120){
+            System.out.println("Sorry, person's age number is too large, please double check. Person record will not change.");
+            return -1;
+            
+            
+        } else{
+            
+            this.age = age;
+            return 1;
+        }
+        
+    }
+    
+    public String toString() {
+        return  "\nName: " + getName() +  "\nEmail: " + getEmail() + "\nAge: " + getAge();
+    }
+    
+}
