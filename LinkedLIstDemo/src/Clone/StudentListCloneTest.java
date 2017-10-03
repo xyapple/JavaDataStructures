33a590dab7e4cd5228d4cf1af11474f6a57973df
xyapple <ydeascentis@gmail.com>
2017-10-03 12:08:19 -0400

    Add a testing file to try out the clone

---
+++ LinkedLIstDemo/src/Clone/StudentListCloneTest.java
@@ -0,0 +1,17 @@
+package Clone;
+
+public class StudentListCloneTest {
+    public static void main(String[] args) {
+    SinglyLinkedList<Student> studentList = new SinglyLinkedList<>();
+        Student student1 = new Student("1", "1", "1", 1, "1", 1);
+        Student student2 = new Student("2", "2", "2", 2, "2", 2);
+        Student student3 = new Student("3", "3", "3", 3, "3", 3);
+        Student student4 = new Student("4", "4", "4", 4, "4", 4);
+        Student student5 = new Student("5", "5", "5", 5, "5", 5);
+        studentList.addFirst(student1);
+        studentList.addFirst(student2);
+        studentList.addFirst(student3);
+        studentList.addFirst(student4);
+        studentList.addFirst(student5);
+    }
+}
