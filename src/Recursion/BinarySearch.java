9f557b513dd73fb78e0335d390add91d0e147210
xyapple <ydeascentis@gmail.com>
2017-10-02 22:53:42 -0400

    call the binary search method and pass into the main

--- src/Recursion/BinarySearch.java
+++ src/Recursion/BinarySearch.java
@@ -6,9 +6,16 @@ import java.util.Scanner;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+/*        Scanner in = new Scanner(System.in);
         System.out.println("please enter a array: ");
-        ArrayList user = new ArrayList();
+        ArrayList user = new ArrayList();*/
+      int[] list ={1,2,3,4,5,7,8,9,10,11,12};
+     
+
+      int t = 11;
+      int low = 0;
+      int high = 11;
+        System.out.println(binarySearch(list,t,low,high));
       
     }
     public static boolean binarySearch(int[] data, int target, int low, int high){
