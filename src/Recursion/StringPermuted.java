8379c92a132ef2bc0501eb59715f3b693d5036c9
xyapple <ydeascentis@gmail.com>
2017-10-04 15:34:18 -0400

    Still having problem with output

--- src/Recursion/StringPermuted.java
+++ src/Recursion/StringPermuted.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class StringPermuted {
     public static void main(String[] args) {
-  /*      Scanner userInput = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         String S = "";
         String U = userInput.nextLine();
-        int k = U.length();*/
-        PrintPermutation(3, "", "ABC");
+        int k = U.length();
+        //PrintPermutation(3, "", "ABC");
+        PrintPermutation(k, S,U);
     }
     public static void PrintPermutation(int k, String S, String U){
         for(int i = 0; i < U.length(); i++){
+            
             String c = U.substring(i, i+1);
             
-            U = U.substring(0, i) + U.substring(i+1, k); //remove the first char of U which meanning only adding the rest of substring
-            System.out.println("what is U: "+U);
-            
+            U = U.substring(0, i) + U.substring(i+1, U.length()); //remove the first char of U which meanning only adding the rest of substring
             S = c + U.substring(0, i);
-    
-            PrintPermutation(k,S,U);
+            if(k == 1){
+                System.out.println(S);
+            } else {
+                PrintPermutation(k,S,U);
+            }
             
            S = S.substring(0, S.length()-1);
            U = U.substring(0, i) + c + U.substring(i);
-    
+        /*   System.out.println("what is U: "+U);
+            System.out.println("what is S: "+S);*/
         }
     }
     
