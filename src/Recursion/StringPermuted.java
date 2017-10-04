d0ced602fd05d96a5b0defb2f271290788b29598
xyapple <ydeascentis@gmail.com>
2017-10-04 15:45:34 -0400

    k needs to be decrease which means i need a base case

--- src/Recursion/StringPermuted.java
+++ src/Recursion/StringPermuted.java
@@ -12,44 +12,27 @@ public class StringPermuted {
         PrintPermutation(k, S,U);
     }
     
-    public static void  PrintPermutation(int k, String S, String U) {
-        
-        for (int i = 0; i < U.length(); i++) {
-            
-            String c = U.substring(i, i + 1); //
-            U = U.substring(0, i) + U.substring(i+1, k); //remove first c from U
-            S += c; //add c to the end of S
-            if(k == 1){
-                System.out.println(S);
-            }
-            else {
-                //recursive
-                PrintPermutation(k-1, S, U);
-                
-            }
-            S = S.substring(0, S.length() - 1); //remove c from the end of S
-            U = U.substring(0, i) + c + U.substring(i); //add c back to U
-            
-        }
-    }
-   /* public static void PrintPermutation(int k, String S, String U){
+ 
+    public static void PrintPermutation(int k, String S, String U){
         for(int i = 0; i < U.length(); i++){
             
             String c = U.substring(i, i+1);
             
             U = U.substring(0, i) + U.substring(i+1, U.length()); //remove the first char of U which meanning only adding the rest of substring
+            //System.out.println("what is U: "+U);
             S += c;
+            //System.out.println("what is S: "+S);
             if(k == 1){
                 System.out.println(S);
             } else {
-                PrintPermutation(k,S,U);
+                PrintPermutation(k-1,S,U);
             }
             
            S = S.substring(0, S.length()-1);
            U = U.substring(0, i) + c + U.substring(i);
-        *//*   System.out.println("what is U: "+U);
-            System.out.println("what is S: "+S);*//*
+          
+           
         }
-    }*/
+    }
     
 }
