b2c327d16037e61a83965f6a6ca62887bc0621f2
xyapple <ydeascentis@gmail.com>
2017-10-04 15:53:12 -0400

    try to come up with a different way

--- src/Recursion/StringPermuted.java
+++ src/Recursion/StringPermuted.java
@@ -4,35 +4,29 @@ import java.util.Scanner;
 
 public class StringPermuted {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+     /*   Scanner userInput = new Scanner(System.in);
         String U = userInput.nextLine();
         String S = "";
-        int k = U.length();
-        //PrintPermutation(3, "", "ABC");
-        PrintPermutation(k, S,U);
+        int k = U.length();*/
+        PrintPermutation(3, "", "ABC");
+        //PrintPermutation(k, S,U);
     }
     
- 
-    public static void PrintPermutation(int k, String S, String U){
-        for(int i = 0; i < U.length(); i++){
-            
-            String c = U.substring(i, i+1);
-            
-            U = U.substring(0, i) + U.substring(i+1, U.length()); //remove the first char of U which meanning only adding the rest of substring
-            //System.out.println("what is U: "+U);
-            S += c;
-            //System.out.println("what is S: "+S);
-            if(k == 1){
-                System.out.println(S);
-            } else {
-                PrintPermutation(k-1,S,U);
+    private static void PrintPermutation(int k,String S, String U) {
+        
+            for (int i = 0; i < k; i++) {
+                // choose
+                String c = U.substring(i, i+1);
+                String newU = U.substring(0, i) + U.substring(i+1, k);        // remove firstLetter from s
+                String newChosen = S + c;
+                if (k == 1) {
+                    System.out.println(S);  // base case
+                } else {
+                    // explore
+                    PrintPermutation(k-1, newU, newChosen);
+                }
             }
-            
-           S = S.substring(0, S.length()-1);
-           U = U.substring(0, i) + c + U.substring(i);
-          
-           
         }
     }
     
-}
+
