c80df99f844c52a25952f80b94fb86477f71b28c
xyapple <ydeascentis@gmail.com>
2017-10-04 15:25:56 -0400

    Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String index out of range: 3

--- src/Recursion/StringPermuted.java
+++ src/Recursion/StringPermuted.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class StringPermuted {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+  /*      Scanner userInput = new Scanner(System.in);
         String S = "";
         String U = userInput.nextLine();
-        int k = U.length();
+        int k = U.length();*/
+        PrintPermutation(3, "", "ABC");
     }
     public static void PrintPermutation(int k, String S, String U){
         for(int i = 0; i < U.length(); i++){
             String c = U.substring(i, i+1);
-            U = U.substring(0, i) + U.substring(i+1, k); //remove 
+            U = U.substring(0, i) + U.substring(i+1, k); //remove the first char of U which meanning only adding the rest of substring
+            System.out.println("what is U: "+U);
+            S = c + U.substring(0, i);
+    
+            PrintPermutation(k,S,U);
             
+           S = S.substring(0, k-1);
+           U = U.substring(0, i) + c + U.substring(i);
+    
         }
     }
     
