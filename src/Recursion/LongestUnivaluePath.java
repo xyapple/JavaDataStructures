4719d940abee2527b5bc00b8274f4c8f91a46d48
xyapple <ydeascentis@gmail.com>
2017-10-05 20:26:13 -0400

    Longest UnivaluePath by calcuate the leftside and rightside the tree

--- src/Recursion/LongestUnivaluePath.java
+++ src/Recursion/LongestUnivaluePath.java
@@ -1,4 +1,5 @@
 package Recursion;
+
 /*leetcode.com
 * Given a binary tree, find the length of the longest path where each node in the path has the same value.
 * This path may or may not pass through the root.
@@ -13,7 +14,10 @@ public class LongestUnivaluePath {
         TreeNode right;
         TreeNode(int x) {val = x;}
     }
-    class Solution{
+    /*
+    * Recusion
+    * */
+   /* class Solution{
         int ans;
         public int longestUnivaluePath(TreeNode root){
             ans = 0;
@@ -29,10 +33,44 @@ public class LongestUnivaluePath {
             int arrowLeft = 0;
             int arrowRight = 0;
             if(node.left != null && node.right.val == node.val){
-                arrowRight = arrowRight + left + 1;
+                arrowLeft = arrowLeft + left + 1;
             }
-            
+            if(node.right != null && node.left.val == node.val){
+                arrowRight = arrowRight + right + 1;
+            }
+            ans = Math.max(ans, arrowLeft+arrowRight);
+            return Math.max(arrowLeft, arrowRight);
             
         }
+      
+    }*/
+   class Solution{
+       public int longestUnivaluePath(TreeNode root){
+           if(root == null) return 0;
+           int val = 0;
+           int result = 0;
+           // calculate the left side of the tree
+           if(root.left != null && root.right.val == val){
+               result = 1 + dfs(root.left);
+           }
+           // calculate the right side of the tree
+           if(root.right != null && root.right.val == val){
+               result = 1 + dfs(root.right);
+           }
+           return Math.max(result, Math.max(longestUnivaluePath(root.left), longestUnivaluePath(root.right)));
+       }
+       public int dfs(TreeNode root){
+           if(root == null) return 0;
+           int val = root.val;
+           int result = 0;
+           if(root.left != null && root.right.val == val){
+               result = 1 + dfs(root.left);
+           }
+           if(root.right != null && root.right.val == val){
+               result = Math.max(result, 1+dfs(root.right));
+           }
+           return result;
+       }
     }
+    
 }
