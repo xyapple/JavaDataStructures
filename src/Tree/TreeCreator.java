c8b19c9b3f998d03b6d9aa11376bc8435669dbe8
xyapple <ydeascentis@gmail.com>
2017-10-10 15:36:26 -0400

    if we know preOrder and inOrder of the tree, how can we get the Tree

--- src/Tree/TreeCreator.java
+++ src/Tree/TreeCreator.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import apple.laf.JRSUIUtils;
+
 public class TreeCreator {
     public TreeNode createSampleTree(){
         TreeNode root = new TreeNode('A');
@@ -9,6 +11,26 @@ public class TreeCreator {
         root.getLeft().getRight().setLeft(new TreeNode('G'));
         root.setRight(new TreeNode('C'));
         root.getRight().setRight(new TreeNode('E'));
+        return root;
+    }
+    public TreeNode createTree(String preOrder, String inOrder){
+        if(preOrder.isEmpty()){
+            return null;
+        }
+        char rootValue = preOrder.charAt(0);
+        int rootIndex = inOrder.indexOf(rootValue);
+        TreeNode root = new TreeNode(rootValue);
+        root.setLeft(
+                createTree(
+                        preOrder.substring(1, 1+rootIndex), //结束就是开始
+                        inOrder.substring(0, rootIndex)
+                ));
+        root.setRight(
+                createTree(
+                        preOrder.substring(1+rootIndex),
+                        inOrder.substring(1+rootIndex)
+                )
+        );
         return root;
     }
 }
