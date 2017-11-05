273928cd7410b56f10ba723dab3b524b37afc410
xyapple <ydeascentis@gmail.com>
2017-11-05 13:50:28 -0500

    write the isUnique method to return

--- src/arraysAndstrings/IsUnique.java
+++ src/arraysAndstrings/IsUnique.java
@@ -1,10 +1,24 @@
 package arraysAndstrings;
 
+import java.util.Arrays;
+
 public class IsUnique {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
+	public boolean isUnique(String test) {
+		char[] c = test.toCharArray();
+		Arrays.sort(c);
+		for(int i = 0; i<c.length; i++) {
+			if(c[i-1]==c[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 }
