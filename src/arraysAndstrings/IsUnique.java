1c80e3eaf7d5dee80ab2636e3b046ddc0eca550a
xyapple <ydeascentis@gmail.com>
2017-11-05 17:59:30 -0500

    finish the unique question

--- src/arraysAndstrings/IsUnique.java
+++ src/arraysAndstrings/IsUnique.java
@@ -5,19 +5,26 @@ import java.util.Arrays;
 public class IsUnique {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
-	}
-	public boolean isUnique(String test) {
-		char[] c = test.toCharArray();
-		Arrays.sort(c);
-		for(int i = 0; i<c.length; i++) {
-			if(c[i-1]==c[i]) {
-				return true;
+		// test the input
+		String t = "safe";
+		isUnique(t);
+	}	
+	public static void isUnique(String test) {
+		boolean[] chars = new boolean[26];
+		String upper = test.toUpperCase();
+		//sort the array before the search
+		for(int i = 0; i<upper.length(); i++) {
+			//create a character array char
+			char c = upper.charAt(i);		
+			if('A' <= c && c<= 'Z') {
+				if(chars[(int)c - 'A']) {
+					System.out.println("not unique");
+					return;
+				}
+				chars[(int)c - 'A'] = true;
 			}
 		}
-		return false;
+		System.out.println("unique");
 	}
 	
 
