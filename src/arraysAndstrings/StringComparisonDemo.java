499cce538bde079c606a555a3e0213447e77affd
xyapple <ydeascentis@gmail.com>
2017-11-06 11:48:35 -0500

    understand the String comparison

---
+++ src/arraysAndstrings/StringComparisonDemo.java
@@ -0,0 +1,39 @@
+package arraysAndstrings;
+
+public class StringComparisonDemo {
+	public static void main(String[] args) {
+		String s1 = "Java isn't just for breakfast.";
+		String s2 = "JAVA isn't just for breakfast.";
+		
+		if (s1.equals(s2))
+		    System.out.println("The two lines are equal.");
+		else if (s2.equals(s1))
+		    System.out.println("The two lines are equal.");
+		else
+		    System.out.println("The two lines are not equal.");
+		System.out.println("=============");
+		
+		if (s1.equalsIgnoreCase(s2))
+		    System.out.println(
+		    "But the lines are equal, ignoring case.");
+		else
+		    System.out.println(
+		    "Lines are not equal, even ignoring case.");
+		String s3 = "A cup of java is a joy forever.";
+		
+		System.out.println("=============");
+		
+		if(s3.compareToIgnoreCase(s1) < 0) {
+			System.out.println("\"" + s3 + "\"");
+			System.out.println("precedes");
+			System.out.println("\"" + s1 + "\"");
+			System.out.println("in alphabetic ordering");
+			
+		} else {
+			System.out.println("s3 does not precede s1.");
+		}
+		
+		
+	}
+
+}
