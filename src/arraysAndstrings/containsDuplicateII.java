b9d81bd6939a95587736cc3aa6ca14e0f8ea9822
xyapple <ydeascentis@gmail.com>
2017-11-06 08:50:29 -0500

    still working on the duplicate problem

--- src/arraysAndstrings/containsDuplicateII.java
+++ src/arraysAndstrings/containsDuplicateII.java
@@ -3,10 +3,26 @@
 //in the array such that nums[i] = nums[j] 
 //and the absolute difference between i and j is at most k.
 package arraysAndstrings;
+import java.util.*;
 
 public class containsDuplicateII {
 	public static void main(String[] args) {
-		
+		int[] numbers = {1,2,3,4};
+		isDuplicate(numbers, 2);
+	}
+	public static boolean isDuplicate(int[] nums, int k) {
+		HashSet<Integer> hm = new HashSet<Integer>();
+		for(int i=0; i <nums.length; i++) {
+			if(i > k) {
+				hm.remove(nums[i-k-1]);
+//				System.out.println("This is duplicate");
+			}
+			if(!hm.add(nums[i])) {
+				System.out.println("This is duplicate");
+				return true;
+			}
+		}
+		return false;
 	}
 	
 
--- src/arraysAndstrings/understandHash.java
+++ src/arraysAndstrings/understandHash.java
@@ -2,6 +2,7 @@ package arraysAndstrings;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.HashMap;
 
 public class understandHash {
 public static void main(String[] args) {
@@ -22,5 +23,17 @@ public static void main(String[] args) {
     
     System.out.println(hs.size() + " distinct words detected.");
     System.out.println(hs);
+    
+    String[] data = new String("Nothing is as easy as it looks").split(" ");
+
+    HashMap <String, Integer> hm = new HashMap<String, Integer>();
+
+    for (String key : data)
+    {
+    	Integer freq = hm.get(key); //returns the value associated with that key. 
+    	if(freq == null) freq = 1; else freq ++;
+    	hm.put(key, freq); //adds the key-value pair to the map.
+    }
+    System.out.println(hm);
 }
 }
