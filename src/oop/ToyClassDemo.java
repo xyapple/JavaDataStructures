fc1d0473da369b7a342c29813c6f7472d7178319
xyapple <ydeascentis@gmail.com>
2017-11-09 19:58:49 -0500

    ToyClass from absolut java reference

--- src/oop/ToyClassDemo.java
+++ src/oop/ToyClassDemo.java
@@ -1,5 +1,13 @@
 package oop;
 
 public class ToyClassDemo {
+	public static void main(String[] args) {
+		
+		ToyClass anObject = new ToyClass("ToyA", 0);
+		System.out.println(anObject);
+		System.out.println("Now we call changer with anObject as argument.");
+		ToyClass.changer(anObject);
+		System.out.println(anObject);
+	}
 
 }
