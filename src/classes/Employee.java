c74f072c515f9fffb9961bed02b8489362c175cc
xyapple <ydeascentis@gmail.com>
2017-11-12 09:51:30 -0500

    add an employee class

--- src/classes/Employee.java
+++ src/classes/Employee.java
@@ -1,9 +1,11 @@
 package classes;
 
 public class Employee {
+	//instance name, startSalary, and timeEmployed
 	private String name;
 	private int startSalary;
 	private double timeEmployed;
 	
 
+	
 }
